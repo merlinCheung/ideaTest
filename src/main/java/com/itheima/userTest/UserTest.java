c5d1efba1a29d968a7ae3d10e10dd9024306a6f5
merlin <22692762@qq.com>
2019-08-02 13:55:36 +0800

    制造冲突

--- src/main/java/com/itheima/userTest/UserTest.java
+++ src/main/java/com/itheima/userTest/UserTest.java
@@ -7,8 +7,8 @@ package com.itheima.userTest;
 public class UserTest {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("111222");
-        System.out.println("6666666");
+        System.out.println("hellaao world");
+        System.out.println("11122sas2");
+        System.out.println("6666ffdd666");
     }
 }
