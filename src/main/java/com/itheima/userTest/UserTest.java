cb6a351449314a0df91840b8e9eb579bd5b925c0
merlin <22692762@qq.com>
2019-08-02 12:33:18 +0800

    first commit

---
+++ src/main/java/com/itheima/userTest/UserTest.java
@@ -0,0 +1,12 @@
+package com.itheima.userTest;
+
+/**
+ * @Author: Merlin cheung
+ * @Date: Creat in 12:29 2019/8/2
+ */
+public class UserTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
